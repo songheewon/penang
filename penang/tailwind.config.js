8a61b0ca1c45ff6282d9adbe38a9b7a1ebcb41f3
songheewon <songheew1020@gmail.com>
2023-07-16 04:49:44 +0900

    penang template

---
+++ penang/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+    require('@tailwindcss/line-clamp'),
+    require('flowbite/plugin'),
+  ],
+}
+
